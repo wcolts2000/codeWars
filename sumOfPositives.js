674ad53ef532cebb9430ca1cf37d09dc022a11bd
wcolts2000 <wcolts2000@gmail.com>
2019-03-28 17:45:38 -0700

    wolfInSheepsClothing

---
+++ sumOfPositives.js
@@ -0,0 +1,23 @@
+// You get an array of numbers, return the sum of all of the positives ones.
+
+// Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+// Note: if there is nothing to sum, the sum is default to 0.
+
+function positiveSum(arr) {
+  if (arr.length === 0) return 0;
+  // console.log(arr);
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] >= 0) {
+      sum += arr[i];
+    }
+  }
+  return sum;
+}
+
+console.log(positiveSum([1, 2, 3, 4, 5]), 15);
+console.log(positiveSum([1, -2, 3, 4, 5]), 13);
+console.log(positiveSum([]), 0);
+console.log(positiveSum([-1, -2, -3, -4, -5]), 0);
+console.log(positiveSum([-1, 2, 3, 4, -5]), 9);
