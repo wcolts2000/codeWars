97c7163c0cb4341ed52f14128fd5baee31e46f83
wcolts2000 <wcolts2000@gmail.com>
2020-02-15 09:06:34 -0800

    noOdds

---
+++ noOdds.js
@@ -0,0 +1,7 @@
+function noOdds(values) {
+  // Return all non-odd values
+  return values.filter(el => el % 2 === 0);
+}
+
+console.log(noOdds([0, 1]), [0]);
+console.log(noOdds([0, 1, 2, 3]), [0, 2]);
