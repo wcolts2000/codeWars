5ccfd1f5e50310884ebbe94268abe804171e3688
wcolts2000 <wcolts2000@gmail.com>
2020-02-16 12:49:40 -0800

    simpleAssmebler

---
+++ simpleAssembler.js
@@ -0,0 +1,101 @@
+function simple_assembler(program) {
+  /* return a dictionary with the registers */
+  const register = {};
+  let currentInstruction = 0;
+  const mov = (a, b) => {
+    if (isNaN(Number(b))) {
+      console.log('yeah');
+      register[a] = register[b];
+      return currentInstruction++;
+    }
+    register[a] = Number(b);
+    currentInstruction++;
+  };
+
+  const inc = a => {
+    register[a] = register[a] + 1;
+    currentInstruction++;
+  };
+
+  const dec = a => {
+    register[a] = register[a] - 1;
+    currentInstruction++;
+  };
+
+  const jnz = (a, step) => {
+    if (register[a] !== 0) {
+      // if (step < 0) {
+      //   currentInstruction -= step
+      // } else {
+      //   currentInstruction += step
+      // }
+      currentInstruction += Number(step);
+      console.log(currentInstruction);
+    } else {
+      currentInstruction++;
+    }
+  };
+
+  const opCodes = {
+    mov,
+    inc,
+    dec,
+    jnz
+  };
+
+  const decode = str => {
+    let currentOperation = str.split(' ');
+    console.log(currentOperation);
+    return currentOperation;
+  };
+
+  while (currentInstruction < program.length) {
+    let op = decode(program[currentInstruction]);
+    console.log(opCodes[op[0]]);
+    opCodes[op[0]](op[1], op[2]);
+    console.log(register);
+    // if (op[0] !== 'jnz')
+    // currentInstruction++
+  }
+
+  return register;
+}
+
+// console.log(
+//   simple_assembler(['mov a 5','inc a','dec a','dec a','jnz a -1', 'inc a']),
+//   {'a': 1}
+// );
+
+console.log(
+  simple_assembler(['mov a -10', 'mov b a', 'inc a', 'dec b', 'jnz a -2']),
+  { a: 0, b: -20 }
+);
+
+// radekk, shivam_jackson solution
+// const asm = {};
+
+// init = () => {
+//   asm.ip = 0;
+//   asm.regs = {};
+// }
+
+// asm.code = {
+//   mov: (x, y) => asm.regs[x] = isNaN(+y) ? +asm.regs[y] : +y,
+//   inc: (x) => asm.regs[x]++,
+//   dec: (x) => asm.regs[x]--,
+//   jnz: (x, y) => (asm.regs[x] !== 0 ? asm.ip += +y - 1 : false)
+// };
+
+// function simple_assembler(program) {
+//   init();
+//   let params = [];
+
+//   while (asm.ip < program.length) {
+//     params = program[asm.ip].split(' ');
+//     asm.code[params[0]].apply(null, params.slice(1));
+
+//     asm.ip++;
+//   }
+
+//   return asm.regs;
+// }
