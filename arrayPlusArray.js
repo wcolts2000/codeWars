f396d6bd014bf22fee3f0ad399f91bb9ae2c10d3
wcolts2000 <wcolts2000@gmail.com>
2020-02-17 11:32:00 -0800

    arrayPlusArray

---
+++ arrayPlusArray.js
@@ -0,0 +1,7 @@
+function arrayPlusArray(arr1, arr2) {
+  return [...arr1, ...arr2].reduce((acc, el) => (acc += el), 0);
+}
+console.log(arrayPlusArray([1, 2, 3], [4, 5, 6]), 21);
+console.log(arrayPlusArray([-1, -2, -3], [-4, -5, -6]), -21);
+console.log(arrayPlusArray([0, 0, 0], [4, 5, 6]), 15);
+console.log(arrayPlusArray([100, 200, 300], [400, 500, 600]), 2100);
