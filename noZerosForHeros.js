424ed016719e904b498a0385737a9105f2d228c8
wcolts2000 <wcolts2000@gmail.com>
2020-02-20 21:13:45 -0800

    noZerosForHeros

---
+++ noZerosForHeros.js
@@ -0,0 +1,28 @@
+// Numbers ending with zeros are boring.
+
+// They might be fun in your world, but not here.
+
+// Get rid of them. Only the ending ones.
+
+// 1450 -> 145
+// 960000 -> 96
+// 1050 -> 105
+// -1050 -> -105
+// Zero alone is fine, don't worry about it. Poor guy anyway
+
+function noBoringZeros(n) {
+  // your code
+  if (n === 0) return 0;
+  let result = n.toString().split('');
+  while (result[result.length - 1] === '0') {
+    result.pop();
+  }
+  return Number(result.join(''));
+}
+
+console.log(noBoringZeros(1450), 145);
+console.log(noBoringZeros(960000), 96);
+console.log(noBoringZeros(1050), 105);
+console.log(noBoringZeros(-1050), -105);
+console.log(noBoringZeros(-105), -105);
+console.log(noBoringZeros(0), 0);
