925eda229b8fd201754d8c9f3c5b4287bdb528f3
wcolts2000 <wcolts2000@gmail.com>
2020-02-25 20:58:30 -0800

    split strings

---
+++ splitStrings.js
@@ -0,0 +1,20 @@
+// Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters
+// then it should replace the missing second character of the final pair with an underscore ('_').
+
+// Examples:
+
+// solution('abc') // should return ['ab', 'c_']
+// solution('abcdef') // should return ['ab', 'cd', 'ef']
+function solution(str) {
+  let ans = [];
+  if (str.length === 0) return ans;
+  if (str.length % 2 === 1) str = str.concat('_');
+  for (let i = 0; i < str.length; i += 2) {
+    ans.push(`${str[i]}${str[i + 1]}`);
+  }
+  return ans;
+}
+
+console.log(solution('abcdef'), ['ab', 'cd', 'ef']);
+console.log(solution('abcdefg'), ['ab', 'cd', 'ef', 'g_']);
+console.log(solution(''), []);
