6860ea3b7ea4372296f328a05435980977e689df
wcolts2000 <wcolts2000@gmail.com>
2020-05-11 08:35:24 -0700

    switch it up

---
+++ switchItUp.js
@@ -0,0 +1,38 @@
+// When provided with a number between 0-9, return it in words.
+
+// Input :: 1
+
+// Output :: "One".
+
+// If your language supports it, try using a switch statement.
+
+function switchItUp(number) {
+ switch (number) {_
+    case 0:
+      return "zero";
+    case 1:
+      return "one";
+    case 2:
+      return "two";
+    case 3:
+      return "three";
+    case 4:
+      return "four";
+    case 5:
+      return "five";
+    case 6:
+      return "six";
+    case 7:
+      return "seven";
+    case 8:
+      return "eight";
+    case 9:
+      return "nine";
+    default:
+      return null;
+  }
+}
+
+console.log(switchItUp(1), 'One');
+console.log(switchItUp(3), 'Three');
+console.log(switchItUp(5), 'Five');
