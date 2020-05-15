3572492b81bbc2071f8e83c4a6c56dd7fea85350
wcolts2000 <wcolts2000@gmail.com>
2020-05-14 17:52:34 -0700

    generate a range of integers

---
+++ whatsWrongWithTheseIdentifiers.js
@@ -0,0 +1,16 @@
+// // An identifier is simply a name...
+// // Can you amend this object so that its properties comprise only vaild identifiers?
+
+// var Person = {
+//   1stname: "John",
+//   second-name: "Doe",
+//   email@ddress: "john.doe@email.com",
+//   male.female: "M"
+// };
+
+var Person = {
+  firstName: 'John',
+  secondName: 'Doe',
+  emailAddress: 'john.doe@email.com',
+  maleFemale: 'M',
+};
