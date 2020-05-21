3cdc8fb5ad3f92d9a6694fdf57b8b2cfc6c25554
wcolts2000 <wcolts2000@gmail.com>
2020-05-20 21:05:20 -0700

    combine strings

---
+++ combineStrings.js
@@ -0,0 +1,13 @@
+// Combine strings function
+// Create a function named combineNames(combine_names in python, ruby, CombineNames in C#) that accepts two
+// parameters (first and last name). The function should return the full name.
+
+// Example:
+
+// combineNames('James', 'Stevens')
+// returns:
+
+// 'James Stevens'
+const combineNames = (n1, n2) => `${n1} ${n2}`;
+
+console.log(combineNames('James', 'Stevens'), 'James Stevens');
