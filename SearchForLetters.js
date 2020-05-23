b17c2e17fa98811f0523a8ed221cd2073c5946e1
wcolts2000 <wcolts2000@gmail.com>
2020-05-23 09:59:40 -0700

    search for letters

---
+++ SearchForLetters.js
@@ -0,0 +1,53 @@
+// Create a function which accepts one arbitrary string as an argument, and return a string of length 26.
+
+// The objective is to set each of the 26 characters of the output string to either '1' or '0' based on the fact
+// whether the Nth letter of the alphabet is present in the input (independent of its case).
+
+// So if an 'a' or an 'A' appears anywhere in the input string (any number of times), set the first character of the
+// output string to '1', otherwise to '0'. if 'b' or 'B' appears in the string, set the second character to '1', and
+// so on for the rest of the alphabet.
+
+// For instance:
+
+// "a   **&  cZ"  =>  "10100000000000000000000001"
+
+function change(string) {
+  const lookup = 'abcdefghijklmnopqrstuvwxyz';
+  let ans = [
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+  ];
+  string = string.toLowerCase();
+  for (let char of string) {
+    if (lookup.indexOf(char) !== -1) {
+      ans[lookup.indexOf(char)] = 1;
+    }
+  }
+  return ans.join('');
+}
+
+console.log(change('a **&  bZ'), '11000000000000000000000001');
